bdaf7778ea48fba8b1e67e7430024a98cdb465ca
Mina Shah <minashah814@gmail.com>
2024-12-11 13:21:35 +0500

    made changes

--- src/components/NoPage.jsx
+++ src/components/NoPage.jsx
@@ -1,6 +1,10 @@
 const NoPage = () => {
     return (
-     <h1 className="text-lg justify-center items-center">404 Error</h1>
+      <>
+      <h1 className="text-lg justify-center items-center">404 Error</h1>
+      <button className="p-2 justify-center items-center"> Back To Home</button>  
+      </>
+    
     )
   };
   
